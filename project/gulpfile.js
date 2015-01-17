e02b1912890158b6a1c5abfe10f6fec6d9e2f568
Chris Bloom <bloomcb@gmail.com>
2015-01-17 14:01:29 -0800

    better folder structure for dev

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -25,14 +25,27 @@ gulp.task('d7:make', ['d7:clean'], function() {
 // Copy local settings.php to new local d7 site
 gulp.task('d7:customFiles', ['d7:make'], function() {
   return gulp.src([
-      'build/config/d7/**/*'
+      'build/dev/d7/**/*'
     ]).pipe(
       gulp.dest('web/drupal/d7/sites/')
     );
 });
 
+// Full drupal install
+gulp.task('d7:install', ['d7:customFiles'], function() {
+  return gulp.src('')
+    .pipe(
+      shell([
+        'drush site-install standard --site-name=DOWNFALLD7 --yes',
+        'drush en downfall_migrate_feature --yes'
+      ], {
+        'cwd':'web/drupal/d7/sites/d7.local.downfallguild.org'
+      })
+    );
+});
+
 // Kick off d7 build
-gulp.task('d7:init', ['d7:customFiles']);
+gulp.task('d7:init', ['d7:install']);
 
 gulp.task('d7:cc', function() {
   return gulp.src('')
@@ -42,8 +55,8 @@ gulp.task('d7:cc', function() {
 });
 
 gulp.task('d7:watch', function() {
-  return gulp.src('build/config/d7/**/*')
-    .pipe(watch('build/config/d7/**/*'))
+  return gulp.src('build/dev/d7/**/*')
+    .pipe(watch('build/dev/d7/**/*'))
     //.pipe(gulp.dest('web/drupal/d7/sites/'));
-    .pipe(shell(['rsync -avzr /var/www/build/config/d7/* /var/www/web/drupal/d7/sites']));
+    .pipe(shell(['rsync -avzr /var/www/build/dev/d7/* /var/www/web/drupal/d7/sites']));
 });
