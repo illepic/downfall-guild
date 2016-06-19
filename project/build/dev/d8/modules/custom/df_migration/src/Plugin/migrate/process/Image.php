03f7e94b6691fc39a55cb3e9dd9fa5174f7298b2
Christopher Bloom <bloomcb@gmail.com>
2016-06-19 01:35:10 -0700

    Image migration now WORKS with custom plugin

---
+++ project/build/dev/d8/modules/custom/df_migration/src/Plugin/migrate/process/Image.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\df_migration\Plugin\migrate\process\Image.
+ */
+
+namespace Drupal\df_migration\Plugin\migrate\process;
+
+use Drupal\Core\Database\Database;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+
+/**
+ * This plugin looks up Drupal 6 Image node fids
+ *
+ * @MigrateProcessPlugin(
+ *   id = "image"
+ * )
+ */
+class Image extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    // Within the D6 Image table, find the D6 fid and return it
+    $image = Database::getConnection('default', 'migrate')->query('SELECT * FROM {image} WHERE nid=:nid', array(':nid' => $value))->fetchObject();
+
+    // Return the D6 fid (so this can be part of a Migrate process chain)
+    return $image->fid;
+  }
+}
