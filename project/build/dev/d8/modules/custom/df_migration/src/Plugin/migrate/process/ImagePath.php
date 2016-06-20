385b42ef0f931397a3558ae2c092689399064bac
Christopher Bloom <bloomcb@gmail.com>
2016-06-20 15:11:02 -0700

    Remove old field_image field from Image content type

---
+++ project/build/dev/d8/modules/custom/df_migration/src/Plugin/migrate/process/ImagePath.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\df_migration\Plugin\migrate\process\ImagePath.
+ */
+
+namespace Drupal\df_migration\Plugin\migrate\process;
+
+use Drupal\Core\Database\Database;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+
+/**
+ * This plugin fixes d6 Image file names to be the actual filename from filesystem
+ *
+ * @MigrateProcessPlugin(
+ *   id = "image_path"
+ * )
+ */
+class ImagePath extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    return basename($value);
+  }
+}
