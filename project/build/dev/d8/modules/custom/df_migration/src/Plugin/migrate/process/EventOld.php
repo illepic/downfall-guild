96f24454737a78fef644153280af9dd90ea4ee68
Christopher Bloom <bloomcb@gmail.com>
2016-06-26 16:38:02 -0700

    Update config to mess around with OpenSocial, old event gets a plugin to help lookup the start/end dates

---
+++ project/build/dev/d8/modules/custom/df_migration/src/Plugin/migrate/process/EventOld.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\df_migration\Plugin\migrate\process\EventOld.
+ */
+
+namespace Drupal\df_migration\Plugin\migrate\process;
+
+use Drupal\Core\Database\Database;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+
+/**
+ * Look up old d6 event start/end dates
+ *
+ * @MigrateProcessPlugin(
+ *   id = "event_old"
+ * )
+ */
+class EventOld extends ProcessPluginBase {
+
+  protected function dateConvert($value) {
+    // Create date object from the raw sting in db. PHP handles this smart
+    $date = new \DateTime($value, new \DateTimeZone('America/Los_Angeles'));
+    // Drupal stores all time in UTC, convert
+    $date->setTimezone(new \DateTimeZone('UTC'));
+    // We need the format 2015-09-18T18:30:00
+    return $date->format('Y-m-d\TH:i:s');
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    $nid = $value;
+    $startend = $this->configuration['startend'];
+
+    // Lookup in the d6 event table for just this nid
+    $date = Database::getConnection('default', 'migrate')
+      ->select('event', 'e')
+      ->fields('e', array('event_start', 'event_end'))
+      ->condition('nid', $nid)
+      ->execute()
+      ->fetchObject();
+
+    return $startend == 'start' ? $this->dateConvert($date->event_start) : $this->dateConvert($date->event_end);
+  }
+}
