1a54d09e89c7f78418eba27f1ca7d3f8ddee633e
Chris Bloom <bloomcb@gmail.com>
2016-09-08 00:17:07 -0700

    Custom source appears to be working

---
+++ project/build/dev/d8/modules/custom/df_migration/src/Plugin/migrate/source/OgContent.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\df_migration\Plugin\migrate\source\OgContent.
+ */
+
+namespace Drupal\df_migration\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+
+/**
+ * Source plugin for OG content
+ *
+ * @MigrateSource(
+ *   id = "og_content"
+ * )
+ */
+
+class OgContent extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = $this->select('og_ancestry', 'a');
+    $query->join('node', 'n', 'a.nid = n.nid');
+    $query
+      ->fields('a', ['nid', 'group_nid'])
+      ->fields('n', ['type', 'title', 'created', 'uid']);
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'nid' => $this->t('NID of content node'),
+      'group_nid' => $this->t('NID of group node'),
+      'type' => $this->t('Type of content node'),
+      'title' => $this->t('Title of content node'),
+      'created' => $this->t('Creation date of content node'),
+      'uid' => $this->t('User ID of content node creator'),
+    ];
+
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'nid' => [
+        'type' => 'integer',
+        'alias' => 'a',
+      ],
+      'group_nid' => [
+        'type' => 'integer',
+        'alias' => 'a',
+      ],
+    ];
+  }
+}
