a4965521a67d9b112664c74aa9a2610340383811
Chris Bloom <bloomcb@gmail.com>
2016-09-24 00:15:14 -0700

    Modal handling for media grids

--- project/build/dev/d8/themes/custom/patternlab/js/script.js
+++ project/build/dev/d8/themes/custom/patternlab/js/script.js
@@ -8,4 +8,15 @@
     }
   };
 
+  Drupal.behaviors.mediaModal = {
+    attach: function(context, settings) {
+      $('#media-modal', context).on('show.bs.modal', function(event) {
+        var $trigger = $(event.relatedTarget);
+        $(this)
+          .find('#modal-media').attr('src', $trigger.data('modal-media')).end()
+          .find('#modal-title').html($trigger.data('modal-title'));
+      });
+    }
+  };
+
 })(jQuery, Drupal);
