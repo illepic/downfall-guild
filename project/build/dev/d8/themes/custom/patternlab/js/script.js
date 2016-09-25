1f15c91abb88ebf4df37fc3ef38f366ac1a5fffa
Chris Bloom <bloomcb@gmail.com>
2016-09-25 00:54:03 -0700

    Rewrite material-kit js because it's shitty

--- project/build/dev/d8/themes/custom/patternlab/js/script.js
+++ project/build/dev/d8/themes/custom/patternlab/js/script.js
@@ -1,13 +1,22 @@
 (function($, Drupal) {
   'use strict';
 
-  Drupal.behaviors.plStarter = {
-    attach: function(context, settings) {
-      $('html', context).addClass('js');
-      console.log('hello');
+  var df = {
+    breakpoints: {
+      sm: 768,
+      md: 992,
+      lg: 1200
+    },
+    parallax: function($element) {
+      var offset = $(window).scrollTop() / 3;
+      $element.isInViewport().css({transform: 'translate3d(0, ' + offset + 'px, 0'});
     }
   };
 
+  /**
+   * Generic media modal with dynamic title and img src
+   *
+   */
   Drupal.behaviors.mediaModal = {
     attach: function(context, settings) {
       $('#media-modal', context).on('show.bs.modal', function(event) {
@@ -15,6 +24,25 @@
         $(this)
           .find('#modal-media').attr('src', $trigger.data('modal-media')).end()
           .find('#modal-title').html($trigger.data('modal-title'));
+      });
+    }
+  };
+
+  /**
+   * page-header parallax
+   */
+  Drupal.behaviors.pageHeaderParallax = {
+    attach: function(context, settings) {
+
+      var $pageHeader = $('.page-header[data-parallax="active"]', context);
+
+      // Only do this for tablet+ or no header
+      if ($(window).width() < df.breakpoints.sm || $pageHeader.length === 0) { return; }
+
+      $(window).on('scroll', function() {
+        window.requestAnimationFrame(function() {
+          df.parallax($pageHeader);
+        });
       });
     }
   };
--- project/build/dev/d8/themes/custom/patternlab/js/vendor/material-kit.js
+++ project/build/dev/d8/themes/custom/patternlab/js/vendor/material-kit.js
@@ -56,13 +56,13 @@ $(document).ready(function(){
         $(window).on('scroll', materialKit.checkScrollForTransparentNavbar)
     }
 
-    if (window_width >= 768){
-        big_image = $('.page-header[data-parallax="active"]');
-        if(big_image.length != 0){
-            $(window).on('scroll', materialKitDemo.checkScrollForParallax);
-        }
-
-    }
+    // if (window_width >= 768){
+    //     big_image = $('.page-header[data-parallax="active"]');
+    //     if(big_image.length != 0){
+    //         $(window).on('scroll', materialKitDemo.checkScrollForParallax);
+    //     }
+    //
+    // }
 
 });
 
@@ -112,62 +112,62 @@ var big_image;
 
 materialKitDemo = {
 
-    checkScrollForParallax: debounce(function(){
-        if(isElementInViewport(big_image)){
-            var current_scroll = $(this).scrollTop();
+    // checkScrollForParallax: debounce(function(){
+        // if(isElementInViewport(big_image)){
+        //     var current_scroll = $(this).scrollTop();
 
-            oVal = ($(window).scrollTop() / 3);
-            big_image.css({
-                'transform':'translate3d(0,' + oVal +'px,0)',
-                '-webkit-transform':'translate3d(0,' + oVal +'px,0)',
-                '-ms-transform':'translate3d(0,' + oVal +'px,0)',
-                '-o-transform':'translate3d(0,' + oVal +'px,0)'
-            });
-        }
-    }, 4),
+            // oVal = ($(window).scrollTop() / 3);
+            // big_image.css({
+            //     'transform':'translate3d(0,' + oVal +'px,0)',
+            //     '-webkit-transform':'translate3d(0,' + oVal +'px,0)',
+            //     '-ms-transform':'translate3d(0,' + oVal +'px,0)',
+            //     '-o-transform':'translate3d(0,' + oVal +'px,0)'
+            // });
+        // }
+    // }, 4),
 
-    initContactUsMap: function(){
-        var myLatlng = new google.maps.LatLng(44.433530, 26.093928);
-        var mapOptions = {
-          zoom: 14,
-          center: myLatlng,
-          styles:
-[{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}],
-          scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
-        }
-        var map = new google.maps.Map(document.getElementById("contactUsMap"), mapOptions);
-
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            title:"Hello World!"
-        });
-        marker.setMap(map);
-    },
-
-    initContactUs2Map: function(){
-        var lat = 44.433530;
-        var long = 26.093928;
-
-        var centerLong = long - 0.025;
-
-        var myLatlng = new google.maps.LatLng(lat,long);
-        var centerPosition = new google.maps.LatLng(lat, centerLong);
-
-        var mapOptions = {
-          zoom: 14,
-          center: centerPosition,
-          styles:
-[{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}],
-          scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
-        }
-        var map = new google.maps.Map(document.getElementById("contactUs2Map"), mapOptions);
-
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            title:"Hello World!"
-        });
-        marker.setMap(map);
-    }
+//     initContactUsMap: function(){
+//         var myLatlng = new google.maps.LatLng(44.433530, 26.093928);
+//         var mapOptions = {
+//           zoom: 14,
+//           center: myLatlng,
+//           styles:
+// [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}],
+//           scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
+//         }
+//         var map = new google.maps.Map(document.getElementById("contactUsMap"), mapOptions);
+//
+//         var marker = new google.maps.Marker({
+//             position: myLatlng,
+//             title:"Hello World!"
+//         });
+//         marker.setMap(map);
+//     },
+//
+//     initContactUs2Map: function(){
+//         var lat = 44.433530;
+//         var long = 26.093928;
+//
+//         var centerLong = long - 0.025;
+//
+//         var myLatlng = new google.maps.LatLng(lat,long);
+//         var centerPosition = new google.maps.LatLng(lat, centerLong);
+//
+//         var mapOptions = {
+//           zoom: 14,
+//           center: centerPosition,
+//           styles:
+// [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}],
+//           scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
+//         }
+//         var map = new google.maps.Map(document.getElementById("contactUs2Map"), mapOptions);
+//
+//         var marker = new google.maps.Marker({
+//             position: myLatlng,
+//             title:"Hello World!"
+//         });
+//         marker.setMap(map);
+//     }
 
 }
 // Returns a function, that, as long as it continues to be invoked, will not
@@ -189,19 +189,19 @@ function debounce(func, wait, immediate) {
 };
 
 
-function isElementInViewport(elem) {
-    var $elem = $(elem);
-
-    // Get the scroll position of the page.
-    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
-    var viewportTop = $(scrollElem).scrollTop();
-    var viewportBottom = viewportTop + $(window).height();
-
-    // Get the position of the element on the page.
-    var elemTop = Math.round( $elem.offset().top );
-    var elemBottom = elemTop + $elem.height();
-
-    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
-}
+// function isElementInViewport(elem) {
+//     var $elem = $(elem);
+//
+//     // Get the scroll position of the page.
+//     var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+//     var viewportTop = $(scrollElem).scrollTop();
+//     var viewportBottom = viewportTop + $(window).height();
+//
+//     // Get the position of the element on the page.
+//     var elemTop = Math.round( $elem.offset().top );
+//     var elemBottom = elemTop + $elem.height();
+//
+//     return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+// }
 
 })(jQuery);
