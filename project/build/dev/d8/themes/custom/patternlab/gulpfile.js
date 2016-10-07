59ae4578e28fd17babfd29551d7770d4a06b4173
Christopher Bloom <bloomcb@gmail.com>
2016-10-06 21:26:52 -0700

    Integrate PL starter 9.1.0

--- project/build/dev/d8/themes/custom/patternlab/gulpfile.js
+++ project/build/dev/d8/themes/custom/patternlab/gulpfile.js
@@ -1,10 +1,7 @@
 'use strict';
 var gulp = require('gulp');
-var yaml = require('js-yaml');
-var fs = require('fs');
 // `rc` allows all config options to be overridden with CLI flags like `--js.enabled=''` or in `~/.p2-theme-corerc` files, among many others: https://www.npmjs.com/package/rc
-var localConfig = yaml.safeLoad(fs.readFileSync('./gulpconfig.yml', 'utf8'));
-var config = require('rc')('p2-theme-core', localConfig);
+var config = require('rc')('p2-theme-core', require('./gulpconfig.js'));
 var tasks = {
   'compile': [],
   'watch': [],
@@ -18,7 +15,7 @@ require('p2-theme-core')(gulp, config, tasks);
 gulp.task('clean', gulp.parallel(tasks.clean));
 gulp.task('compile', gulp.series(
   'clean',
-  tasks.compile
+  gulp.series(tasks.compile)
 ));
 gulp.task('validate', gulp.parallel(tasks.validate));
 gulp.task('watch', gulp.parallel(tasks.watch));
